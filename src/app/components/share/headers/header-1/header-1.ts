b173aa8f5b5508c1a51ae65b4ac5df6ee03dbe23
carlosOlcina <carlosolcina23@gmail.com>
2025-12-31 19:07:47 +0100

    feat(share): implement header 1 component

---
+++ src/app/components/share/headers/header-1/header-1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-1',
+  imports: [],
+  templateUrl: './header-1.html',
+  styleUrl: './header-1.css',
+})
+export class Header1 {}
